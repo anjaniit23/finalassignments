32d819725c1da52ba11775258392978cbbba9cb5
anjaniit23 <anjaniit23@gmail.com>
2021-08-10 09:05:26 +0530

    added

--- configure/keys_prod.js
+++ configure/keys_prod.js
@@ -1,10 +1,6 @@
 require("dotenv").config({path : "../config.env"});
-module.exports ={
-    mongoURI:"mongodb+srv://anjani:anjani@cluster0.2cyzb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    keysecret:"secret"
-};
 
-// module.exports ={
-//     mongoURI:process.env.MONGO_URI,
-//     keysecret:process.env.SECRET_OR_KEY
-// };
+module.exports ={
+    mongoURI:process.env.MONGO_URI,
+    keysecret:process.env.SECRET_OR_KEY
+};
