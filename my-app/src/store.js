ae9494f86f28618cc98fbce25628356f78b1cd88
anjaniit23 <anjaniit23@gmail.com>
2021-08-10 15:44:16 +0530

    added

--- my-app/src/store.js
+++ my-app/src/store.js
@@ -1,18 +1,33 @@
+
 import { applyMiddleware, createStore,compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/index';
+require("dotenv").config({path : "../../config.env"});
 
 const initialState={};
-
+var store;
 
 const middleware=[thunk];
 
-const store=createStore( 
-    rootReducer  ,
-    initialState ,
-    compose(
-        applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-        )
-    );//1st parameter is reducer
+if(process.env.NODE_ENV === 'production') {
+ 
+     store = createStore(
+        rootReducer, 
+        initialState,
+         compose(
+        applyMiddleware(...middleware)
+    ));
+}else{
+
+     store = createStore( 
+        rootReducer  ,
+        initialState ,
+        compose(
+            applyMiddleware(...middleware),
+            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+            )
+        );//1st parameter is reducer
+    
+}
+
 export default store;
